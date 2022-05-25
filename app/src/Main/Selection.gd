a7c805e9230cd0671bdf6c78e0c3e66a67f350ad
nikolai-in <nikolai.in@tuta.io>
2022-05-25 07:43:45 +0300

    :construction: Layers selection

--- app/src/Main/Selection.gd
+++ app/src/Main/Selection.gd
@@ -1,2 +1,17 @@
 extends State
 
+onready var tree: = get_node("../../UI/Sidebar/ScrollContainer/Column/Editor/Margin/Column/Layers/Tree")
+
+var selection = []
+
+
+func _on_Tree_multi_selected(item: TreeItem, column: int, selected: bool) -> void:
+	var origin = item.get_metadata(column)
+	if origin is Polygon2D:
+		if selected:
+			origin.self_modulate = Color( 2, 2, 2, 0.99 )
+			selection.append(origin)
+		else:
+			origin.self_modulate = Color( 1, 1, 1, 1 )
+			selection.remove(selection.find(origin))
+	print(selection)
--- app/src/UI/Editor/Layers/LayersTree.gd
+++ app/src/UI/Editor/Layers/LayersTree.gd
@@ -2,24 +2,33 @@ extends Tree
 
 onready var MapCanvas : Node2D = owner.get_node("MapCanvas")
 
+var selected: TreeItem
+var origin_color: Color
 
-func make_root(node: Node) -> TreeItem:
-	var root: TreeItem = create_item()
-	root.set_text(0, node.name)
-	root.set_metadata(0, node)
-	return root
+var names = []
+var root: TreeItem
+
+func _ready() -> void:
+	root = create_item()
+	root.set_text(0, MapCanvas.name)
+	root.set_metadata(0, MapCanvas)
+	
 
 func update() -> void:
-	clear()
-	var root = make_root(MapCanvas)
 	for element in MapCanvas.get_children():
-		var item: = create_item(root)
-		item.set_text(0, element.name)
-		item.set_metadata(0, element)
+		if not element.name in names:
+			names.append(element.name)
+			var item: = create_item(root)
+			item.set_text(0, element.name)
+			item.set_metadata(0, element)
 
 
 func _on_Tree_item_selected() -> void:
-	var selected: = get_selected()
+	if selected && selected.get_metadata(0) is Polygon2D:
+		selected.get_metadata(0).color = origin_color
+	selected = get_selected()
+	print_debug(selected)
 	var origin = selected.get_metadata(0)
 	if origin is Polygon2D:
+		origin_color = origin.color
 		origin.color = Color.red
