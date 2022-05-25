2fe0487c8c41d7268d6e48e4beb4cd7a2214671d
Николай Иванов <tameddev@Nikolajs-MacBook-Pro-2.local>
2022-05-25 10:42:00 +0300

    :zap: Improve layers code

--- app/src/UI/Editor/Layers/LayersTree.gd
+++ app/src/UI/Editor/Layers/LayersTree.gd
@@ -5,19 +5,11 @@ onready var MapCanvas : Node2D = owner.get_node("MapCanvas")
 var selected: TreeItem
 var origin_color: Color
 
-var names = []
-var root: TreeItem
-
-func _ready() -> void:
-	root = create_item()
-	root.set_text(0, MapCanvas.name)
-	root.set_metadata(0, MapCanvas)
-	
 
 func update() -> void:
+	clear()
+	var root: TreeItem = create_item(self)
 	for element in MapCanvas.get_children():
-		if not element.name in names:
-			names.append(element.name)
-			var item: = create_item(root)
-			item.set_text(0, element.name)
-			item.set_metadata(0, element)
+		var item: = create_item(root)
+		item.set_text(0, element.name)
+		item.set_metadata(0, element)
